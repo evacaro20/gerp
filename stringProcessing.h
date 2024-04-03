649335e1792e1057bb69cb3a77eab8be36e7fe76
Eva Caro <159644972+evacaro20@users.noreply.github.com>
2024-04-03 16:15:55 -0400

    Update stringProcessing.h

--- stringProcessing.h
+++ stringProcessing.h
@@ -1,14 +1,11 @@
 /*
  * stringProcessing.h
  * 
- * COMP 15 Project 4: Gerp
- * 
  * The declaration of the stripNonAlphaNum function. This function parses
  * through a string, getting rid of the non alphanumeric characters at 
  * the front and back.
  * 
- * Edited by Eva Caro 
- * 12/02/2023
+ * Edited 12/02/2023
  */
 
 #include <string>
